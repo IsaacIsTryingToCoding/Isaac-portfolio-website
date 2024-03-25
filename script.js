f58650931968a02d285606c1552980cf34e4422a
Isaac <149269461+IsaacIsTryingToCoding@users.noreply.github.com>
2024-03-25 14:36:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener("click", function() {
     document.body.classList.toggle('open');
 });
+
+let menu_btn = document.querySelector('.hamburger');
+menu_btn.addEventListener('click', function() {
+    menu_btn.classList.toggle('is-active')
+})
